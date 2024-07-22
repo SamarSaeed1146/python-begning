2dabe9cdced720b44f32035d73f76112b7f6cc79
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-07-22 19:00:28 +0500

    variable type hints cheat

---
+++ 51) variable type hints cheat/demo.py
@@ -0,0 +1,18 @@
+# a: str = "Samar SAeed"
+# b: int = "Samar Saeed"
+# c: int = 18
+# d : float = 22
+# e: bool = "false"
+
+
+# y: list[int] = [1,2]
+# z: list[str] = ["samar", "saeed"]
+
+# y: set[int] = [1,2]
+# z: set[str] = {"samar", "saeed"}
+
+# y: dict[str, float] = {"field": 2.0}
+# z: dict[str, str] = {"name": "samar", "age": "18"}
+
+x: tuple[int, str, float] = (3, "yes", 7.5)
+y: tuple[str, str, str] = ("3", "yes", "7.5")
