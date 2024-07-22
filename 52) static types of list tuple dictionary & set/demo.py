a822047811ff4e0d940e1558cc64b00c7cfcddeb
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-07-22 23:02:33 +0500

    static types list,tuple,dictionary & set

---
+++ 52) static types of list tuple dictionary & set/demo.py
@@ -0,0 +1,13 @@
+from typing import List,Dict,Tuple 
+
+# a: List[str] = ["Samar","Abdul",]
+
+# b: Dict[str,str] = {"name":"Samar Saeed","age":"18"}
+
+# c: Dict[int,str] = { 1 : "one" }
+
+# d: Tuple[str,str,float] = ("Samar", "2", 5.2) 
+
+a: set[str] = {"Samar", "Hamzah"}
+
+b: set[int] = {55, 24}
